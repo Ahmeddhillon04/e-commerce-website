8a1c8a019bc9f33553c44baf34a39c6c19182565
Ahmed Dhillon <ahmeddhillon1@gmail.com>
2023-11-16 20:35:20 -0500

    initial commit

---
+++ src/Recommended/Recommended.js
@@ -0,0 +1,26 @@
+import"./Recommended.css";
+import Buttons from "../components/Buttons";
+
+function Recommended({handleClick}) {
+    return (
+      <>
+      <div>
+        <h2 className = "recommended-title" >Recommended</h2>
+        <div className = "recommended-flex">
+        <Buttons onClickHandler = {handleClick}
+           value = "" title = "All Products"></Buttons>
+          <Buttons onClickHandler = {handleClick}
+           value = "Nike" title = "Nike"></Buttons>
+            <Buttons onClickHandler = {handleClick}
+           value = "Adidas" title = "Adidas"></Buttons>
+            <Buttons onClickHandler = {handleClick}
+           value = "Puma" title = "Puma"></Buttons>
+            <Buttons onClickHandler = {handleClick}
+           value = "Vans" title = "Vans"></Buttons>
+        </div>
+      </div>
+      </>
+    );
+  }
+  
+  export default Recommended;
